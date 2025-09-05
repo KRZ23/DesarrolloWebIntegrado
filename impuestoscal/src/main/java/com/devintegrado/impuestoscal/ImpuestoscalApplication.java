a8e3d009dcc25c1a82bb159eeababc50748d108d
Cristopher Rivera <cristopherspier23@gmail.com>
2025-09-04 20:54:04 -0500

    Inicio del proyecto
    Bases del proyecto

---
+++ impuestoscal/src/main/java/com/devintegrado/impuestoscal/ImpuestoscalApplication.java
@@ -0,0 +1,13 @@
+package com.devintegrado.impuestoscal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpuestoscalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpuestoscalApplication.class, args);
+	}
+
+}
---
+++ impuestoscal/src/test/java/com/devintegrado/impuestoscal/ImpuestoscalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devintegrado.impuestoscal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpuestoscalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
